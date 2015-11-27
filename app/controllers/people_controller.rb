0f289f806b1b3a9af59c0a04097ab358506e6b2b
Jorge Mata <llamiro@tovarisch.local>
2015-11-26 20:37:17 -0600

    Modified list view to comply with description

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,7 +4,7 @@ class PeopleController < ApplicationController
   # GET /people
   # GET /people.json
   def index
-    @people = Person.all
+    @people = Person.all.order(:first_name, :last_name)
   end
 
   # GET /people/1
--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,6 @@
 module PeopleHelper
+	def calculate_age(date)
+    now = Time.now.utc.to_date
+    now.year - date.year - ((now.month > date.month || (now.month == date.month && now.day >= date.day)) ? 0 : 1)
+  end
 end
